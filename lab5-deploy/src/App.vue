9d9d2886a7dcfc5e51b7260c5b908c813bb8ac07
Varintor <varintorn_si@cmu.ac.th>
2025-08-08 00:11:38 +0700

    re deploy

---
+++ lab5-deploy/src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import { useMessageStore } from '@/stores/message'
+import { storeToRefs } from 'pinia'
+const store = useMessageStore()
+const { message } = storeToRefs(store)
+
+</script>
+
+<template>
+  <div class="text-center font-sans text-gray-700 antialias">
+    <header>
+      <div id="flashMessage" class="animate-fade" v-if="message">
+        <h4>{{ message }}</h4>
+      </div>
+      <div class="wrapper">
+        <nav class="py-6">
+          <RouterLink class="font-bold text-gray-700" :to="{name: 'event-list-view'}">Event</RouterLink> | 
+          <RouterLink class="font-bold text-gray-700" :to="{name: 'about'}">About</RouterLink> |
+          <RouterLink class="font-bold text-gray-700" :to="{name: 'students'}">Student</RouterLink>
+        </nav>
+        <div class="page-size-selector">
+          <span>Page Size: </span>
+          <RouterLink :to="{ name: 'event-list-view', query: { page: 1, size: 2 }}">2</RouterLink> |
+          <RouterLink :to="{ name: 'event-list-view', query: { page: 1, size: 3 }}">3</RouterLink> |
+          <RouterLink :to="{ name: 'event-list-view', query: { page: 1, size: 4 }}">4</RouterLink> 
+        </div>
+      </div>
+    </header>
+
+    <RouterView />
+  </div>
+</template>
