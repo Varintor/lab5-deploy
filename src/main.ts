9a3c2fd79f1efd67c3a2a965c3831251b5081044
Varintor <varintorn_si@cmu.ac.th>
2025-08-08 00:55:29 +0700

    s

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import { createPinia } from 'pinia'
 import './assets/style.css'
 import App from './App.vue'
 import router from './router'
-import   'nprogress/nprogress.css'
+import 'nprogress/nprogress.css'
+import { inject } from '@vercel/analytics'
 
 const app = createApp(App)
-
+inject()
 app.use(createPinia())
 app.use(router)
 
